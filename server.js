c42b49052d676462ef2d2d067adc3c8e857b0762
sengathit <sengathit.l@gmail.com>
2020-01-05 12:10:44 -0600

    connecting to postgresql

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const path = require('path')
+const { Client } = require('pg')
+const client = new Client()
 
 const app = express()
 
@@ -8,7 +10,13 @@ const port = process.env.PORT || 3001
 app.use(express.static(path.join(__dirname,'public')))
 
 app.get('',(req,res) => {
-    res.sendFile('index.html')
+    // res.sendFile('index.html')
+    client.connect()
+
+    client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+        console.log(err ? err.stack : res.rows[0].message) // Hello World!
+        client.end()
+    })
 })
 
 app.listen(port,() => console.log('Server on port ' + port))
