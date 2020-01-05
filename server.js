518334e2df85150a29937e0e0eab955e5aed39bf
sengathit <sengathit.l@gmail.com>
2020-01-05 12:51:11 -0600

    adding database config

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3001
 app.use(express.static(path.join(__dirname,'public')))
 
 app.get('',(req,res) => {
-    res.send('hey')
+    res.sendFile('index.html')
 })
 
 app.get('/users',(req,res) => {
