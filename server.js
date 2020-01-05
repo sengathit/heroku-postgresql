c95dc3f7d1e1bfe1b8fe6c3f25338059f7df2ca9
sengathit <sengathit.l@gmail.com>
2020-01-05 12:35:32 -0600

    test route

--- server.js
+++ server.js
@@ -10,23 +10,11 @@ const port = process.env.PORT || 3001
 app.use(express.static(path.join(__dirname,'public')))
 
 app.get('',(req,res) => {
-    // res.sendFile('index.html')
-    client.connect()
-
-    client
-        .query('SELECT NOW() as now')
-        .then(response => res.send(response.rows[0]))
-        .catch(e => console.error(e.stack))
+    res.send('hey')
 })
 
 app.get('/users',(req,res) => {
-    // res.sendFile('index.html')
-    client.connect()
-
-    client
-        .query('SELECT NOW() as now')
-        .then(response => res.send(response.rows[0]))
-        .catch(e => console.error(e.stack))
+    res.send('kaka')
 })
 
 app.listen(port,() => console.log('Server on port ' + port))
